631ce7034adeb8dffe6aee8fcc55bbc997ebfa7b
Łukasz Włódarczyk <lukasz.wlodarczyk@tapptic.com>
2017-08-05 00:40:08 +0200

    added heartbeat scheduling

--- src/main/java/com/createam/api/Createam.java
+++ src/main/java/com/createam/api/Createam.java
@@ -3,8 +3,10 @@ package com.createam.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Createam {
 
 	public static void main(String[] args) {
--- src/main/java/com/createam/api/service/HeartbeatService.java
+++ src/main/java/com/createam/api/service/HeartbeatService.java
@@ -1,6 +1,9 @@
 package com.createam.api.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,18 +16,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Scope("singleton")
 public class HeartbeatService {
 
+    private static final Logger log = LoggerFactory.getLogger(HeartbeatService.class);
+
     private LocalDateTime lastHeartbeat = LocalDateTime.now();
     private AtomicInteger counter = new AtomicInteger(0);
 
+    @Scheduled(fixedRate = 5000)
     public LocalDateTime updateAndGetLast() {
         LocalDateTime temp = lastHeartbeat;
         lastHeartbeat = LocalDateTime.now();
         counter.incrementAndGet();
+        log.info("beat!");
         return temp;
     }
+
 
     public Integer getCount() {
         lastHeartbeat = LocalDateTime.now();
         return counter.incrementAndGet();
     }
+
 }
