07096fda4f709625e036ff47751cb15cd408fe73
Łukasz Włódarczyk <lukasz.wlodarczyk@tapptic.com>
2017-08-07 09:59:21 +0200

    added user createion method

--- src/main/java/com/createam/api/controller/UserController.java
+++ src/main/java/com/createam/api/controller/UserController.java
@@ -4,11 +4,9 @@ import com.createam.api.model.User;
 import com.createam.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -32,5 +30,11 @@ public class UserController {
     List<User> getAll() {
         return StreamSupport.stream(userRepository.findAll().spliterator(), false)
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody User create(@RequestBody User user) {
+        user.setRegistered(new Date());
+        return userRepository.save(user);
     }
 }
