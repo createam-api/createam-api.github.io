7309503f16d4c75ba1a6dd13bad6b5a60dae83d9
Łukasz Włódarczyk <lukasz.wlodarczyk@tapptic.com>
2017-08-07 13:16:12 +0200

    added test heroku env property

---
+++ src/main/java/com/createam/api/config/properties/HerokuProperties.java
@@ -0,0 +1,21 @@
+package com.createam.api.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by lukasz@create.am on 07/08/2017.
+ */
+@Component
+@PropertySource("classpath:application-shared.properties")
+@Getter
+@Setter
+public class HerokuProperties {
+
+    @Value("${test}")
+    private String test;
+
+}
--- src/main/java/com/createam/api/controller/HeartbeatController.java
+++ src/main/java/com/createam/api/controller/HeartbeatController.java
@@ -1,5 +1,6 @@
 package com.createam.api.controller;
 
+import com.createam.api.config.properties.HerokuProperties;
 import com.createam.api.model.Heartbeat;
 import com.createam.api.service.HeartbeatService;
 import org.slf4j.Logger;
@@ -19,10 +20,12 @@ public class HeartbeatController {
 
     private static final AtomicInteger COUNTER = new AtomicInteger(0);
     private final HeartbeatService heartbeatService;
+    private final HerokuProperties herokuProperties;
 
     @Autowired
-    public HeartbeatController(HeartbeatService heartbeatService) {
+    public HeartbeatController(HeartbeatService heartbeatService, HerokuProperties herokuProperties) {
         this.heartbeatService = heartbeatService;
+        this.herokuProperties = herokuProperties;
     }
 
     @GetMapping(value = "/heartbeat", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -30,5 +33,10 @@ public class HeartbeatController {
     Heartbeat heartbeat(HttpServletRequest request) {
         log.info("Received heartbeat from " + request.getRemoteAddr());
         return heartbeatService.generateHeartbeat(request);
+    }
+
+    @GetMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String test() {
+        return herokuProperties.getTest();
     }
 }
